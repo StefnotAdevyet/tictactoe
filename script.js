1e81fa1a441c901171cf538c1be0c16664a1156a
stefnotadevyet <srjack91@gmail.com>
2021-07-26 15:10:02 -0400

    a draw game triggers an endgame

--- script.js
+++ script.js
@@ -35,19 +35,29 @@ function handleClick(e) {
 
   if (checkWin(currentClass)) {
     endGame(false)
+  } else if (isDraw()) {
+    endGame(true)
+  } else {
+    swapTurns();
+    setBoardHoverClass()
   }
 
-  swapTurns();
-  setBoardHoverClass()
+
 }
 
 function endGame(draw) {
   if (draw) {
-
+    winningMessageTextElement.innerText = "DRAW!"
   } else {
     winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} win!`
   }
   winningMessageElement.classList.add('show')
+}
+
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  })
 }
 
 function placeMark(cell, currentClass) {
